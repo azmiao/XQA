a1c71e234aed306f43671271f9168fccbcf91efb
azmiao <2362020227@qq.com>
2023-03-26 15:07:23 +0800

    解决回答中不用于随机回答的\#

--- util.py
+++ util.py
@@ -31,7 +31,7 @@ SPLIT_INTERVAL = 1      # 消息分段发送时间间隔，只能数字，单位
 IS_FORWARD = False      # 开启后将使用转发消息发送，默认关闭
 
 # 设置问答的时候，是否校验回答的长度，最大长度和上方 MSG_LENTH 保持一致（可随时改动，重启BOT生效）
-IS_JUDGE_LENTH = True   # 校验回答的长度，在长度范围内就允许设置问题，超过就不允许，默认开启
+IS_JUDGE_LENTH = False   # 校验回答的长度，在长度范围内就允许设置问题，超过就不允许，默认开启
 
 # 如果开启分段发送，且长度没超限制，且开启转发消息时，由于未超长度限制只有一条消息，这时是否需要直接发送而非转发消息（可随时改动，重启BOT生效）
 IS_DIRECT_SINGER = True # 直接发送，默认开启
@@ -154,6 +154,8 @@ async def adjust_img(bot, str_raw: str, is_ans: bool = False, save: bool = False
         else:
             # 如果是保存问答的时候，就只替换图片的路径，其他CQ码的替换相当于没变
             str_raw = str_raw.replace(cqcode[0], f'[CQ:{cqcode[1]},{cqcode[2]}={raw_body}]')
+    # 解决回答中不用于随机回答的\#
+    flit_msg = flit_msg.replace('\#', '#')
     return str_raw if not is_ans else flit_msg
 
 
